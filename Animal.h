7b8697fa32ad91ef523167ba3670ddb1b08fc3d4
Luís Travassos <96450753+Luyiz333@users.noreply.github.com>
2022-11-26 18:15:16 +0000

    animal update

--- Animal.h
+++ Animal.h
@@ -44,14 +44,35 @@ public:
 
 class Coelho: public Animal{
 public:
-    Coelho(int id, int r, int c): Animal('c',id, r, c){};
+    Coelho(int id, int r, int c): Animal('c',id, r, c, 4, 0, 0, 0, 0, 1, "verdura", 10, 8, 1){};
+    //void chanceWalk();
+    //void decrNutri();
+};
+
+class Ovelha: public Animal{
+public:
+    Ovelha(int id, int r, int c): Animal('c',id, r, c, 3, 0, 0, 0, 0, 1, "erva", 15, 15, 4){};
     //void chanceWalk();
     //void decrNutri();
 };
 
 class Lobo: public Animal{
 public:
-    Lobo(int id, int r, int c): Animal('l',id, r, c){};
+    Lobo(int id, int r, int c): Animal('c',id, r, c, 5, 0, 0, 0, 0, 2, "carne", 15, 5, 10){};
+    //void chanceWalk();
+    //void decrNutri();
+};
+
+class Canguru: public Animal{
+public:
+    Canguru(int id, int r, int c): Animal('c',id, r, c, 7, 0, 0, 0, 0, 1, "nothing", 0, 30, 15){};
+    //void chanceWalk();
+    //void decrNutri();
+};
+
+class MisteryAnimal: public Animal{
+public:
+    MisteryAnimal(int id, int r, int c): Animal('c',id, r, c, 4, 0, 0, 0, 0, 1, "verdura", 10, 8, 1){};
     //void chanceWalk();
     //void decrNutri();
 };
