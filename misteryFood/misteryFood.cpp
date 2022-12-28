59feed0b9c091405f6eeb6500f5318f48a0f4377
Pedro Jorge <pedrorodriguesjorge03@gmail.com>
2022-12-28 20:08:37 +0000

    Mystery Food done

--- misteryFood/misteryFood.cpp
+++ misteryFood/misteryFood.cpp
@@ -1,13 +1,14 @@
 #include "misteryFood.h"
+//Erva venenosa
 
 misteryFood::misteryFood(int i, int row, int col) : Food(i, row, col) {
     this->type = 'a';
     this->id = i;   this->row = row;
-    this->col = col;  this->toxicity = 0;
+    this->col = col;  this->toxicity = 10;
     this->nutriValue = 0;
-    this->duration = 0;
-    this->smell_1 = "nothing";
-    this->smell_2 = "nothing";
+    this->duration = 30;
+    this->smell_1 = "erva";
+    this->smell_2 = "verdura";
 }
 
 bool misteryFood::die() {
