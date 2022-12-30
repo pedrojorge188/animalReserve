7a21fca9932fa673577cb48347895072101d7cea
Luís Travassos <96450753+Luyiz333@users.noreply.github.com>
2022-12-30 00:06:48 +0000

    Update Animal.cpp

--- Animal.cpp
+++ Animal.cpp
@@ -66,38 +66,29 @@ void Animal::setHistory(string food, int tox, int nutr) {
     ostringstream buf;
     if(nEntries == 0){
         oldHistory = new string;
-        cout << "1";
     }else if(nEntries > 0){
         newHistory = new string[nEntries];
-        cout << "2";
         for (int i = 0; i < nEntries; ++i) {
             newHistory[i] = oldHistory[i];
-            cout << "3";
         }
         if(nEntries==1){
             delete oldHistory;
-            cout << "4A";
         }else{
             delete [] oldHistory;
-            cout << "4B";
         }
 
         oldHistory = new string[nEntries+1];
-        cout << "5";
         for (int i = 0; i < nEntries; ++i) {
             oldHistory[i] = newHistory[i];
-            cout << "6";
         }
         delete [] newHistory;
-        cout << "7";
     }
 
     buf << "-> " << food << ", tox: " << tox << " , nutr: " << nutr << endl;
 
     oldHistory[nEntries] = buf.str();
-    cout << "8";
     nEntries++;
-    cout << "9";
+
 }
 
 string Animal::printHistory() const {
